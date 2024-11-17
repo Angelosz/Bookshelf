b7b9d6d4393ba43adabb46ab5a28efd2155e595b
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-17 22:58:14 +0000

    Extracted Loading & Error Screens from PortraitLayout

---
+++ app/src/main/java/angelosz/bookshelf/ui/ErrorScreen.kt
@@ -0,0 +1,22 @@
+package angelosz.bookshelf.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorMessage(message: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = message
+        )
+    }
+}
--- app/src/main/java/angelosz/bookshelf/ui/PortraitLayout.kt
+++ app/src/main/java/angelosz/bookshelf/ui/PortraitLayout.kt
@@ -1,16 +1,12 @@
 package angelosz.bookshelf.ui
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
@@ -30,9 +26,8 @@ fun PortraitLayout(
 
     when(uiState){
         is BookshelfUIState.Success -> {
-            val _uiState = uiState as BookshelfUIState.Success
-            if(_uiState.isDetailView){
-                _uiState.currentBook?.let {
+            if(uiState.isDetailView){
+                uiState.currentBook?.let {
                     BookDetailView(
                         book = it,
                         modifier = modifier.padding(dimensionResource(R.dimen.padding_medium))
@@ -55,26 +50,11 @@ fun PortraitLayout(
             }
         }
         is BookshelfUIState.BookshelfError -> {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(
-                    text = uiState.message ?: "There was an error :("
-                )
-            }
+            ErrorMessage(uiState.message ?: "There was an error :(")
         }
         BookshelfUIState.BookshelfLoading -> {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(
-                    text = "Loading..."
-                )
-            }
+            LoadingMessage()
         }
     }
 }
+

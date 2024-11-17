232d3a11968dc026883e0af602c6ae8f5ce6bd79
Angel Cabrera Gil <angelcabgil@gmail.com>
2024-11-17 18:22:29 +0000

    Adds call to AppLayout to the MainActivity

--- app/src/main/java/angelosz/bookshelf/MainActivity.kt
+++ app/src/main/java/angelosz/bookshelf/MainActivity.kt
@@ -6,17 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
 import angelosz.bookshelf.ui.theme.BookshelfTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BookshelfTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
-
+                    AppLayout(windowSize = calculateWindowSizeClass(this).widthSizeClass)
                 }
             }
         }
